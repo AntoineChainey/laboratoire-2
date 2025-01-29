be5c92c6c5ea884fed4c60d046077cd3545c6040
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-01-29 15:00:12 -0500

    Create excercisse 6.py

---
+++ excercisse 6.py
@@ -0,0 +1,4 @@
+nombre = input("Entrez un nombre: ")# les deux point doivent être coller
+nombre = float(nombre) # il faut convertir le type de variable en float ou en entier
+resultat = nombre * 2
+print("Le double de votre nombre est :", (resultat))
