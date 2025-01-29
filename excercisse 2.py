0814bf68c834d75484c2abddddb06436756b3689
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-01-29 13:27:59 -0500

    Create excercisse 2.py

---
+++ excercisse 2.py
@@ -0,0 +1,6 @@
+#demande a l'utilisateur d'entrer un nombre
+nombre = input("entrez un nombre : ")
+nombre = int(nombre) # convertir nombre en entier
+y = float(nombre)  # convertir variable float 
+print ( nombre)
+print ( y )# affichez les résultats
