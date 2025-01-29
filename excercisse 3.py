63570ef56c06895b2f331b7c10e6a9a1653d7487
AntoineChainey <202487791@cegepsherbrooke.qc.ca>
2025-01-29 13:37:26 -0500

    Create excercisse 3.py

---
+++ excercisse 3.py
@@ -0,0 +1,4 @@
+def fonction (a,b) : # definir var a et b locale
+ résultat = a+b # définir var résultat
+ return résultat  # retourner var a la fonction
+print (fonction(3,4)) # montrer le résultat 
